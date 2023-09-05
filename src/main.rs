81815709ef3ac656546042de4a2559f42a6f095b
EvanRoedema <evanroedema71@gmail.com>
2023-09-06 02:46:13 +0300

    ilk yorum

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+trait Özellik {
+    fn yaz(&self) {
+       
+    }
+}
+
+struct Kare{
+    kenar: u8
+}
+
+impl Özellik for Kare {
+    
+}
+
+struct Dikdörtgen{
+    uzun: u8,
+    kısa: u8
+}
+
+impl Özellik for Dikdörtgen {
+    
+}
+
+fn main() {
+    
+}
